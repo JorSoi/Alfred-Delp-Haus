9f627a1de7acf4b98d425eb97e7c0cfb55e867f9
Jorim Soika <jorim.soika@gmx.de>
2021-04-15 18:27:34 +0200

    global: navbar adjustments;  bewerbung: accordeon fix

--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,142 @@ AOS.init();
 
 
 
+
+
+
+
+// Navbar shadow on scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("scroll-shadow").style.boxShadow = "0px 1px 11px #00000034";
+  } else {
+    document.getElementById("scroll-shadow").style.boxShadow = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
 // Opening and Closing mobile menu
 const hamburgerIcon = document.getElementById('hamburger-icon');
 const menuCloser = document.getElementById('menu-closer');
 const mobileOpened = document.getElementById('mobile-opened');
+document.body.style.overflow = 'unset';
+
 
 const opening = () =>{
     mobileOpened.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    document.body.style.height = '100%';
+    document.body.style.width = '100%';
 }
 
 const closing = () => {
     mobileOpened.style.display = 'none';
+    document.body.style.overflow = 'unset';
 }
 
 hamburgerIcon.addEventListener('click', opening);
 menuCloser.addEventListener('click', closing);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -399,141 +520,47 @@ testimonialArrowRightMobile.addEventListener('click', goRight);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// page: bewerbung q&a accordeon
-const text1 = document.getElementById('text-q1');
-const text2 = document.getElementById('text-q2');
-const text3 = document.getElementById('text-q3');
-const text4 = document.getElementById('text-q4');
-const text5 = document.getElementById('text-q5');
-
-const question1 = document.querySelector('.question-1');
-const question2 = document.querySelector('.question-2');
-const question3 = document.querySelector('.question-3');
-const question4 = document.querySelector('.question-4');
-const question5 = document.querySelector('.question-5');
-
-const arrowIcon1 = document.querySelector('.icon-1');
-const arrowIcon2 = document.querySelector('.icon-2');
-const arrowIcon3 = document.querySelector('.icon-3');
-const arrowIcon4 = document.querySelector('.icon-4');
-const arrowIcon5 = document.querySelector('.icon-5');
-
-
-//Event Handlers for each Text field
-const toggleText1 = () => {
-    if(text1.style.display === 'none') {
-    text1.style.display = 'block';
-    arrowIcon1.style.transform = 'rotate(180deg)';
-    text1.style.marginTop = '0';
-    text2.style.display = 'none';
-    text3.style.display = 'none';
-    text4.style.display = 'none';
-    text5.style.display = 'none';
-    arrowIcon2.style.transform = '';
-    arrowIcon3.style.transform = '';
-    } else {
-        text1.style.display = 'none';
-        arrowIcon1.style.transform = '';
-    }
-};
-
-const toggleText2 = () => {
-    if(text2.style.display === 'none') {
-    text2.style.cssText = 'display: block; margin-top: 0';
-    arrowIcon2.style.transform = 'rotate(180deg)';
-    text1.style.display = 'none';
-    text3.style.display = 'none';
-    arrowIcon1.style.transform = '';
-    arrowIcon3.style.transform = '';
-    } else {
-        text2.style.display = 'none';
-        arrowIcon2.style.transform = '';
-    }
-};
-
-const toggleText3 = () => {
-    if(text3.style.display === 'none') {
-    text3.style.display = 'block';
-    arrowIcon3.style.transform = 'rotate(180deg)';
-    text3.style.marginTop = '0';
-    text1.style.display = 'none';
-    text2.style.display = 'none';
-    arrowIcon1.style.transform = '';
-    arrowIcon2.style.transform = '';
-    } else {
-        text3.style.display = 'none';
-        arrowIcon3.style.transform = '';
-    }
-};
-
-const toggleText4 = () => {
-    if(text4.style.display === 'none') {
-    text4.style.display = 'block';
-    arrowIcon4.style.transform = 'rotate(180deg)';
-    text3.style.marginTop = '0';
-    text1.style.display = 'none';
-    text2.style.display = 'none';
-    arrowIcon1.style.transform = '';
-    arrowIcon2.style.transform = '';
-    } else {
-        text3.style.display = 'none';
-        arrowIcon3.style.transform = '';
-    }
-};
-
-const toggleText5 = () => {
-    if(text3.style.display === 'none') {
-    text3.style.display = 'block';
-    arrowIcon3.style.transform = 'rotate(180deg)';
-    text3.style.marginTop = '0';
-    text1.style.display = 'none';
-    text2.style.display = 'none';
-    arrowIcon1.style.transform = '';
-    arrowIcon2.style.transform = '';
-    } else {
-        text3.style.display = 'none';
-        arrowIcon3.style.transform = '';
-    }
-};
-
-
-
-
-
-
-question1.addEventListener('click', test);
-question2.addEventListener('click', toggleText2);
-question3.addEventListener('click', toggleText3);
-question4.addEventListener('click', toggleText4);
-question5.addEventListener('click', toggleText5);
-
-const test = () => {
-    alert('it works');
+const openTestimonial1 = () => {
+    profileIndex = 0;
+    selectProfile();
 }
+
+const openTestimonial2 = () => {
+    profileIndex = 1;
+    selectProfile();
+}
+
+const openTestimonial3 = () => {
+    profileIndex = 2;
+    selectProfile();
+}
+
+testimonialNav1.addEventListener('click', openTestimonial1);
+testimonialNav2.addEventListener('click', openTestimonial2);
+testimonialNav3.addEventListener('click', openTestimonial3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
